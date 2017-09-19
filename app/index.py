5f1c2cf7e8c6a2dd65a9b24a7712b5a269ed18c8
HMCodingSnowMan <hansmarasigan@gmail.com>
2017-09-19 00:32:09 -0700

    thinking msgbox into ynbox into button box

--- app/index.py
+++ app/index.py
@@ -1,8 +1,24 @@
 import easygui;
 print('starting');
 
-msg = "Enter your personal information"
-title = "Credit Card Application"
-fieldNames = ["Name","Street Address","City","State","ZipCode"]
+msg = "Enter your search Query, Then push OK"
+title = "Item Finder"
+fieldNames = ["Item Query 1","Item Query 2","Item Query 3","Item Query 4","Item Query 5"]
 fieldValues = []  # we start with blanks for the values
 fieldValues = easygui.multenterbox(msg,title, fieldNames)
+
+# make sure that none of the fields was left blank
+while 1:
+    if fieldValues is None: break
+    errmsg = ""
+    for i in range(len(fieldNames)):
+        if fieldValues[i].strip() == "":
+            errmsg += ('"%s" is a required field.\n\n' % fieldNames[i])
+    if errmsg == "":
+        break # no problems found
+    fieldValues = easygui.multenterbox(errmsg, title, fieldNames, fieldValues)
+
+print("Reply was: %s" % str(fieldValues))
+
+result = easygui.enterbox(msg='Enter something.', title='fire', default='', strip=True, image=None, root=None)
+print(result)
