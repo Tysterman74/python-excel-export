9ed17a1ef729dd99eb9d2c85f13c6bdb706fa9b6
HMCodingSnowMan <hansmarasigan@gmail.com>
2017-09-19 17:17:14 -0700

    1st window for getting the query, 2nd window for saying we're searching, 3rd window for asking if they want it exported as excel

--- app/index.py
+++ app/index.py
@@ -1,24 +1,38 @@
 import easygui;
 print('starting');
 
-msg = "Enter your search Query, Then push OK"
-title = "Item Finder"
-fieldNames = ["Item Query 1","Item Query 2","Item Query 3","Item Query 4","Item Query 5"]
-fieldValues = []  # we start with blanks for the values
-fieldValues = easygui.multenterbox(msg,title, fieldNames)
+def searchBoxGo():
+    msg = "Enter a search Query, Then push OK"
+    title = "Item Finder"
+    fieldValues = []  # we start with blanks for the values
+    fieldValues = easygui.enterbox(msg,title, fieldValues)
 
-# make sure that none of the fields was left blank
-while 1:
-    if fieldValues is None: break
-    errmsg = ""
-    for i in range(len(fieldNames)):
-        if fieldValues[i].strip() == "":
-            errmsg += ('"%s" is a required field.\n\n' % fieldNames[i])
-    if errmsg == "":
-        break # no problems found
-    fieldValues = easygui.multenterbox(errmsg, title, fieldNames, fieldValues)
+    # make sure that none of the fields was left blank
+    while 1:
+        if fieldValues is None: break
+        errmsg = ""
+        for i in range(len(fieldValues)):
+            if fieldValues[i].strip() == "":
+                errmsg += ('"%s" is a required field.\n\n' % fieldValues[i])
+        if errmsg == "":
+            break # no problems found
+        fieldValues = easygui.enterbox(errmsg, title, fieldValues)
 
-print("Reply was: %s" % str(fieldValues))
+    #print("Reply was: %s" % str(fieldValues))
+    return fieldValues;
 
-result = easygui.enterbox(msg='Enter something.', title='fire', default='', strip=True, image=None, root=None)
-print(result)
+        
+def msgBoxGo(fieldValue):
+    msgWindow="Searching for %s" %str(fieldValue)
+    easygui.msgbox(msgWindow) #message window saying we are searching for the search term
+
+def ynBoxGo():
+    ynboxQuestion="Would you like to export as an Excel ?"
+    ynboxTitle="ExportExcel"
+    ynResult=easygui.ynbox(ynboxQuestion,ynboxTitle)
+    #print(ynResult)
+
+searchBoxResult=searchBoxGo()
+msgBoxGo(searchBoxResult)
+ynBoxGo()
+    
