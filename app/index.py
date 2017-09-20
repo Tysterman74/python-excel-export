8e7ec72c3e1486ecc47aefc0680dd4e49f68c938
Tysterman <tynutsathitya@gmail.com>
2017-09-19 22:25:01 -0700

    Created server

--- app/index.py
+++ app/index.py
@@ -1,8 +1,19 @@
-import easygui;
-print('starting');
+import easygui
+import window
+import requests
+from flask import Flask
 
-msg = "Enter your personal information"
-title = "Credit Card Application"
-fieldNames = ["Name","Street Address","City","State","ZipCode"]
-fieldValues = []  # we start with blanks for the values
-fieldValues = easygui.multenterbox(msg,title, fieldNames)
+app = Flask(__name__)
+
+@app.route('/')
+
+def index():
+    baseurl = 'http://m.newegg.com/ProductList?description=LCD+TV&categoryId=411' + \
+          '&storeId=10&nodeId=7719&parentCategoryId=264&isSubCategory=true&' + \
+          'categoryType=1'
+    testResponse = requests.get(baseurl).content
+    print(testResponse)
+    return testResponse;
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- app/window.py
+++ app/window.py
@@ -1,13 +1,2 @@
-import pyforms;
-from   pyforms          import BaseWidget;
-from   pyforms.Controls import ControlText;
-from   pyforms.Controls import ControlButton;
-
-class DataScraperWindow(BaseWidget):
-    def __init__(self):
-        super(DataScraperWindow, self).__init__('Data finder');
-
-        self._test = ControlText('Test', 'Default Value');
-
-if __name__ == "__main__":
-    pyforms.start_app(DataScraperWindow);
+def printString(test):
+    print(test);
