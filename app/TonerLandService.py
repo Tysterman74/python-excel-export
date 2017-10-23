3926b2443b63a9c596b1d8c72da4a1b8d524130b
HMCodingSnowMan <hansmarasigan@gmail.com>
2017-10-22 19:20:46 -0700

    json not working. trying to figure out how to find the right thing to parse for the pieces

--- app/TonerLandService.py
+++ app/TonerLandService.py
@@ -13,7 +13,8 @@ class TonerLandService:
 # 
 # 
 #
-    
+    keyword="var models"
+
     def createUrlArray(self):
         baseUrl = 'http://www.tonerland.com/brother/'
         seriesArray = ['dcp-series.html', 'fax-series.html', 'hl-series.html', 'intellifax-series.html', 'mfc-series.html'] 
@@ -27,9 +28,42 @@ class TonerLandService:
         response = requests.get('file:///base.html',timeout=5)
         return response.text
 
+    def parseItemContainer(self, container):
+        santizing=container.split()
+        return santizing
+
+    def findKeyword(self,word):
+        if word==self.keyword:
+            return True
+        else:
+            return False
+
+    def containsKeyWord(self,array):
+        for item in array:
+            if self.findKeyword(item):
+                print(item)
+            
+                
+                
+
+
     def findContainers(self):
         with open("base.html") as fp:
-            sanitized = BeautifulSoup(fp)
+            sanitized = BeautifulSoup(fp, 'html.parser')
             #sanitized.encode(fp.encoding)
-            itemContainers= json.loads(sanitized.find_all('script', type='text/javascript').text)
-            return itemContainers['url']
+            itemContainers=(sanitized.find_all('script', type = "text/javascript"))
+            itemLen=len(itemContainers)
+            for item in itemContainers:
+                temp=item.text
+                newtemp=temp.split()
+                if (len(newtemp)>0):
+                    print(len(newtemp))
+                    print(newtemp)
+
+                # self.containsKeyWord(newtemp)
+            print("not "+self.keyword)
+            return itemLen
+
+    
+
+
