dddb01bd5e46e7d0ffc6cf9e287477e0c9add239
HMCodingSnowMan <hansmarasigan@gmail.com>
2017-10-24 19:36:28 -0700

    trying to fix merge error

--- app/index.py
+++ app/index.py
@@ -1,33 +1,24 @@
 import easygui
 import NewEggService
-from TonerLandService import TonerLandService
+import ExportModule
 from flask import Flask
 from searchWindows import searchWindows
-
-
 
 app = Flask(__name__)
 
 @app.route('/')
 
-# def index():
-#     testResponse = NewEggService.GetResults()
-#     for excelObj in testResponse:
-#         print(excelObj.name)
-#         print(excelObj.price)
-#         print(excelObj.url)
-#         print(excelObj.vendor)
-#     return 'asdf'
-  
 def index():
-    tls = TonerLandService()
-    #test = tls.createUrlArray()
-    #results=tls.GetResults()
-    # print(test)
-    results = tls.findContainers()
-    #results=tls.openLocal()
-    print(results)
-    return 'orange'
+    NewEggService.TestUrl()
+    # ExportModule.TestCreate()
+    # response = NewEggService.GetResults()
+    # for excelObj in response:
+    #     print(excelObj.name)
+    #     print(excelObj.price)
+    #     print(excelObj.url)
+    #     print(excelObj.vendor)
+    # ExportModule.create_excel_sheet_from_data(response, 'dicks_in_my_mouth')
+    return 'asdf'
 
 # searchwindow=searchWindows()
 # searchwindow.runAllWindows()
@@ -36,3 +27,12 @@ if __name__ == '__main__':
     app.run(debug=True)
 print('starting')
 
+# def index():
+    # tls = TonerLandService()
+    # #test = tls.createUrlArray()
+    # #results=tls.GetResults()
+    # # print(test)
+    # results = tls.findContainers()
+    # #results=tls.openLocal()
+    # print(results)
+    # return 'orange'
